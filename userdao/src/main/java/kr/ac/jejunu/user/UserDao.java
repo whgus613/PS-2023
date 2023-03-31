a0be1dfaa602f1afcf67a1eb155b4b7b88f94a05
whgus613 <79959287+whgus613@users.noreply.github.com>
2023-03-31 18:06:10 +0900

    3/31

--- userdao/src/main/java/kr/ac/jejunu/user/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -1,47 +1,57 @@
 package kr.ac.jejunu.user;
 
-import kr.ac.jejunu.user.User;
-
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
 
-    private final ConnectionMaker connectionMaker;
+    private final DataSource dataSource;
 
-    public UserDao(ConnectionMaker connectionMaker){
-        this.connectionMaker = connectionMaker;
+    public UserDao(DataSource dataSource){
+        this.dataSource = dataSource;
     }
 
     public User findById(Long id) throws ClassNotFoundException, SQLException {
-        //데이터 어디에 있나? mysql
-        //localhost jeju   => jeju/jejupw => userinfo
-        //jdbc driver 클래스 로딩
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        //커넥션 을 맺고
-        Connection connection = DriverManager.getConnection
-                ("jdbc:mysql://localhost/jeju", "root", "guswl1008");
-        //sql 을 만들고
-        PreparedStatement preparedStatement = connection.prepareStatement
-                ("select id, name, password from userinfo where id = ?");
-        preparedStatement.setLong(1, id);
-        //sql 을 실행하고
-        ResultSet resultSet = preparedStatement.executeQuery();
-        //결과를 user 에 잘 매핑하고요
-        resultSet.next();
-        User user = new User();
-        user.setId(resultSet.getLong("id"));
-        user.setName(resultSet.getString("name"));
-        user.setPassword(resultSet.getString("password"));
-        //자원을 해지
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        User user;
+
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = connection.prepareStatement
+                    ("select id, name, password from userinfo where id = ?");
+            preparedStatement.setLong(1, id);
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            user = new User();
+            user.setId(resultSet.getLong("id"));
+            user.setName(resultSet.getString("name"));
+            user.setPassword(resultSet.getString("password"));
+        } finally{
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         //결과를 리턴
         return user;
     }
 
     public void insert(User user) throws ClassNotFoundException, SQLException{
-        Connection connection = connectionMaker.getConnection();
+        Connection connection = dataSource.getConnection();
 
         PreparedStatement preparedStatement = connection.prepareStatement("insert into userinfo (name, password) values (?, ?)", Statement.RETURN_GENERATED_KEYS);
         preparedStatement.setString(1, user.getName());
--- userdao/src/test/java/kr/ac/jejunu/userDaoTests.java
+++ userdao/src/test/java/kr/ac/jejunu/userDaoTests.java
@@ -1,7 +1,10 @@
 package kr.ac.jejunu;
 
 import kr.ac.jejunu.user.*;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
@@ -10,14 +13,23 @@ import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
 
 public class userDaoTests {
+    private static UserDao userDao;
+    @BeforeAll
+    public static void setup(){
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
+        userDao = applicationContext.getBean("userDao", UserDao.class);
+    }
     @Test
     public void get() throws SQLException, ClassNotFoundException {
         Long id = 1l;
         String name = "Jay";
         String password = "1234";
         //UserDao userDao = new UserDao();
-        ConnectionMaker connectionMaker = new JejuConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
+//        ConnectionMaker connectionMaker = new JejuConnectionMaker();
+//        UserDao userDao = new UserDao(connectionMaker);
+//
+//        DaoFactory daoFactory = new DaoFactory();
+//        UserDao userDao = daoFactory.getUserDao();
 
         User user = userDao.findById(id);
         assertThat(user.getId(), is(id));
@@ -32,46 +44,9 @@ public class userDaoTests {
         User user = new User();
         user.setName(name);
         user.setPassword(password);
-        ConnectionMaker connectionMaker=new JejuConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
-        userDao.insert(user);
-        assertThat(user.getId(), greaterThan(1l));
 
-        User insertedUser = userDao.findById(user.getId());
-        assertThat(insertedUser.getId(),  is(user.getId()));
-        assertThat(insertedUser.getName(), is(name));
-        assertThat(insertedUser.getPassword(), is(password));
-    }
-
-    //한라대
-    @Test
-    public void getForHalla() throws SQLException, ClassNotFoundException {
-    // UserDao userDao = new HallaConnectionMaker();
-        ConnectionMaker connectionMaker = new HallaConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
-
-        //DB에 작성된 내용으로 지정
-        Long id = 1l;
-        String name  = "Jay";
-        String password = "1234";
-
-        User user = userDao.findById(id);
-        assertThat(user.getId(),is(id));
-        assertThat(user.getName(),is(name));
-        assertThat(user.getPassword(),is(password));
-    }
-
-    @Test
-    public void insertForHalla() throws SQLException, ClassNotFoundException {
-        String name = "조현지";
-        String password = "1111";
-        User user = new User();
-        user.setName(name);
-        user.setPassword(password);
-
-    //  UserDao userDao = new HallaConnectionMaker();
-        ConnectionMaker connectionMaker = new HallaConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
+//        DaoFactory daoFactory = new DaoFactory();
+//        UserDao userDao = daoFactory.userDao();
 
         userDao.insert(user);
         assertThat(user.getId(), greaterThan(1l));
@@ -81,5 +56,44 @@ public class userDaoTests {
         assertThat(insertedUser.getName(), is(name));
         assertThat(insertedUser.getPassword(), is(password));
     }
+
+//    //한라대
+//    @Test
+//    public void getForHalla() throws SQLException, ClassNotFoundException {
+//    // UserDao userDao = new HallaConnectionMaker();
+//        ConnectionMaker connectionMaker = new HallaConnectionMaker();
+//        UserDao userDao = new UserDao(connectionMaker);
+//
+//        //DB에 작성된 내용으로 지정
+//        Long id = 1l;
+//        String name  = "Jay";
+//        String password = "1234";
+//
+//        User user = userDao.findById(id);
+//        assertThat(user.getId(),is(id));
+//        assertThat(user.getName(),is(name));
+//        assertThat(user.getPassword(),is(password));
+//    }
+//
+//    @Test
+//    public void insertForHalla() throws SQLException, ClassNotFoundException {
+//        String name = "조현지";
+//        String password = "1111";
+//        User user = new User();
+//        user.setName(name);
+//        user.setPassword(password);
+//
+//    //  UserDao userDao = new HallaConnectionMaker();
+//        ConnectionMaker connectionMaker = new HallaConnectionMaker();
+//        UserDao userDao = new UserDao(connectionMaker);
+//
+//        userDao.insert(user);
+//        assertThat(user.getId(), greaterThan(1l));
+//
+//        User insertedUser = userDao.findById(user.getId());
+//        assertThat(insertedUser.getId(),  is(user.getId()));
+//        assertThat(insertedUser.getName(), is(name));
+//        assertThat(insertedUser.getPassword(), is(password));
+//    }
 
 }
