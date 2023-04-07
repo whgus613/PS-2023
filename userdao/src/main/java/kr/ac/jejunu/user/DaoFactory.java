d8008955c2f0f727311f9538a2269a4c6e885252
whgus613 <79959287+whgus613@users.noreply.github.com>
2023-04-07 18:21:23 +0900

    4/7 finish

--- userdao/src/main/java/kr/ac/jejunu/user/DaoFactory.java
+++ userdao/src/main/java/kr/ac/jejunu/user/DaoFactory.java
@@ -1,6 +1,5 @@
 package kr.ac.jejunu.user;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,11 +8,8 @@ import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import javax.sql.DataSource;
 import java.sql.Driver;
 
-
 @Configuration
 public class DaoFactory {
-
-
     @Value("${db.username}")
     private String username;
     @Value("${db.password}")
@@ -23,22 +19,25 @@ public class DaoFactory {
     @Value("${db.url}")
     private String url;
 
-  @Bean
+    @Bean
     public UserDao userDao() throws ClassNotFoundException {
-        UserDao userDao = new UserDao(dataSource());
+        UserDao userDao = new UserDao(jdbcContext());
         return userDao;
     }
 
     @Bean
-    public DataSource dataSource() throws ClassNotFoundException{
+    public JdbcContext jdbcContext() throws ClassNotFoundException {
+        JdbcContext jdbcContext = new JdbcContext(dataSource());
+        return jdbcContext;
+    }
 
+    @Bean
+    public DataSource dataSource() throws ClassNotFoundException {
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-        dataSource.setDriverClass((Class<? extends Driver>)Class.forName(className));
+        dataSource.setDriverClass((Class<? extends Driver>) Class.forName(className));
         dataSource.setUrl(url);
         dataSource.setUsername(username);
         dataSource.setPassword(password);
-
         return dataSource;
-
     }
 }
