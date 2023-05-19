cf8d4a64ec3ac17c8c4bfab6ef7d36e70cbbc35b
whgus613 <79959287+whgus613@users.noreply.github.com>
2023-05-19 17:19:03 +0900

    content negotiation view resolver

--- userdao/src/main/java/kr/ac/jejunu/user/UserController.java
+++ userdao/src/main/java/kr/ac/jejunu/user/UserController.java
@@ -4,14 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping
 @RequiredArgsConstructor
 public class UserController {
     private final UserDao userDao;
 
-    @RequestMapping
+    @RequestMapping(path = "/user", produces = "text/html")
     public User getUser(@RequestParam Long id) {
         return userDao.findById(id);
     }
